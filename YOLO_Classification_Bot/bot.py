b620325aa1228d53ebd5917945564661ce3f6105
wolderufael <wolderuphaelkassahun@gmail.com>
2024-11-18 16:20:00 +0300

    Telegram Bot Deployment with render

--- YOLO_Classification_Bot/bot.py
+++ YOLO_Classification_Bot/bot.py
@@ -5,13 +5,13 @@ from telegram.ext import (
 )
 from dotenv import load_dotenv
 import cv2
-import os
+import numpy as np
 from ultralytics import YOLO
 
 
 # Load environment variables once
 load_dotenv('.env')
-telegram_bot_token = os.getenv('TOKEN2')
+telegram_bot_token = os.getenv('TOKEN4')
 
 # Initialize the Application
 app = Application.builder().token(telegram_bot_token).build()
@@ -26,11 +26,18 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 async def handle_photo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     # Download the photo
     photo_file = await update.message.photo[-1].get_file()
-    photo_path = await photo_file.download_to_drive()
+    # photo_path = await photo_file.download_to_drive()
 
-    # Load image with OpenCV
-    img = cv2.imread(photo_path)
-    model=YOLO("models/last.pt")
+    # # Load image with OpenCV
+    # Download the image as a byte array
+    photo_bytes = await photo_file.download_as_bytearray()
+    
+    # Convert the byte array to a NumPy array for OpenCV
+    image_np = np.frombuffer(photo_bytes, np.uint8)
+    
+    # Decode the image using OpenCV
+    img = cv2.imdecode(image_np, cv2.IMREAD_COLOR)
+    model=YOLO("../models/last.pt")
 
     results=model(img)
     def get_category_name(results):
@@ -51,13 +58,13 @@ async def handle_photo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
 app.add_handler(CommandHandler("start", start))
 app.add_handler(MessageHandler(filters.PHOTO, handle_photo))  # Photo handler
 
-# # Run the webhook for the bot
-# app.run_webhook(
-#     listen="0.0.0.0",
-#     port=int(os.environ.get('PORT', 5001)),
-#     url_path=telegram_bot_token,
-#     webhook_url=f'https://dictionary-bot-pbld.onrender.com/{telegram_bot_token}'
-# )
+# Run the webhook for the bot
+app.run_webhook(
+    listen="0.0.0.0",
+    port=int(os.environ.get('PORT', 5001)),
+    url_path=telegram_bot_token,
+    webhook_url=f'https://dictionary-bot-pbld.onrender.com/{telegram_bot_token}'
+)
 
 # # Start the bot
-app.run_polling()
+# app.run_polling()
